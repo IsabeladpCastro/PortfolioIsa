10517d9c2c9f3e9d33cbb803df8f68c95b2c191d
Isabela Castro <isabeladpcastro@gmail.com>
2024-02-16 23:07:31 -0300

    Finalização da Pagina Home

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ MenuBtn.addEventListener('click', function(e)
         this.classList.toggle('fa-xmark')
     }
 )
+
+let typed = new Typed('.auto-input',{
+    strings: ['Computer Science Student!', 'Front-End Developer!', 'UI Designer!'],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
